1471f914f1eb1979a03cc7a428c727fcf7515874
Christopher Meyers <Joe.Master@att.net>
2014-05-07 20:50:24 -0700

    user agent name fix
    for tests only, more polite to reddit server

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import praw
 
 class Tests(unittest.TestCase):
   def test_praw(self):
-    r = praw.Reddit(user_agent='r/starcraft event tracker')
+    r = praw.Reddit(user_agent='r/allthingszerg replay flair bot')
     frontpage = r.get_front_page()
     self.assertEqual(sum(1 for _ in frontpage), 25)
 
