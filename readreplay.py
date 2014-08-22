04202e80856948559a433d6048853f5ee0d821d3
Christopher Meyers <Joe.Master@att.net>
2014-08-22 14:43:59 -0700

    invert if to reduce nesting
    pure refactoring

--- readreplay.py
+++ readreplay.py
@@ -104,19 +104,20 @@ def handleMessage(message):
     return False
 
   regionName = RegionNameFromId(regionInt)
-  if regionName != None:
-    playerBnetUrl = '{0}/1/{1}/'.format(playerInt,playerName)
-    leagueData = funcs.getLeague(settings.regions[regionName], playerBnetUrl)
-    if leagueData:
-        f = open("accounts.txt","a")
-        f.write('{0},{1},{2},\n'.format(playerBnetUrl, redditname, regionName))
-        f.close()
-        r.set_flair(subreddit, redditname, playerName, leagueData[0].title() + " "+regionName+" " + leagueData[1] + "-" + leagueData[2] + "-" + leagueData[3])
-        messageReply(message,"Your flair has been set.  Account link is a success!")
-    else:
-      messageReply(message,"Error: {DD6B39E6-857C-11E3-9693-7A7328D43830}")
-  else:
+  if regionName == None:
     messageReply(message,"Your region is not supported.  Go yell at the programmer responsible")
+    return False
+
+  playerBnetUrl = '{0}/1/{1}/'.format(playerInt,playerName)
+  leagueData = funcs.getLeague(settings.regions[regionName], playerBnetUrl)
+  if leagueData:
+      f = open("accounts.txt","a")
+      f.write('{0},{1},{2},\n'.format(playerBnetUrl, redditname, regionName))
+      f.close()
+      r.set_flair(subreddit, redditname, playerName, leagueData[0].title() + " "+regionName+" " + leagueData[1] + "-" + leagueData[2] + "-" + leagueData[3])
+      messageReply(message,"Your flair has been set.  Account link is a success!")
+  else:
+    messageReply(message,"Error: {DD6B39E6-857C-11E3-9693-7A7328D43830}")
 
 
 
