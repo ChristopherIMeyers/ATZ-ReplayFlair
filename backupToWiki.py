e059a07487f5cd31ac06d33155643c7652341e5f
Chris Meyers <Chris@NullRecursion.com>
2017-03-15 17:40:01 -0700

    backupToWiki working using PRAW 4.4.0

--- backupToWiki.py
+++ backupToWiki.py
@@ -6,14 +6,13 @@ import funcs
 
 allAccountMaps = funcs.readAccountsFile('accounts.txt')
 
-r = praw.Reddit(user_agent='ATZ flair script! Pipe Battle.Net data to Reddit')
-r.login(settings.reddituser, settings.redditpass, disable_warning=True)
-subreddit = r.get_subreddit('AllThingsZerg')
+r = funcs.GetPraw()
+subreddit = r.subreddit('AllThingsZerg')
 
 def accountRowToWiki(accountRow):
   return '    ' + accountRow['bnet'] + ',' + accountRow['redditName'] + ',' + accountRow['region']
 
 wikiContent = '\n'.join(map(accountRowToWiki, allAccountMaps))
 
-r.edit_wiki_page(subreddit, 'flairlist', wikiContent, 'beep boop - backing up account data')
+subreddit.wiki['flairlist'].edit(wikiContent, 'beep boop - backing up account data')
 
